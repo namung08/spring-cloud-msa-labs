b8e4cf9527c69c2fb8ff2aaee6f89e8588210ad1
namung08 <namung08@gmail.com>
2025-08-07 15:12:38 +0900

    :zap: perf: Repository 인터페이스 개선

--- backend/user-service/src/main/java/com/sesac/msa/userservice/repository/UserRepository.java
+++ backend/user-service/src/main/java/com/sesac/msa/userservice/repository/UserRepository.java
@@ -2,10 +2,10 @@ package com.sesac.msa.userservice.repository;
 
 import java.util.Optional;
 
-import org.springframework.data.repository.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sesac.msa.userservice.entity.User;
 
-public interface UserRepository extends Repository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findById(Long id);
 }
