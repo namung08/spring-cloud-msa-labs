1942b234c722dfcab14900fa2a9ea58a2b75be4e
namung08 <namung08@gmail.com>
2025-08-08 15:37:44 +0900

    :zap: perf: RequestMapping -> BASE_URL 로 설정
     - OpenFeign 을 사용하여 요청을 하게 될 경우 @RequestMapping 을 통해하는 것이 아니라 수정

--- backend/order-service/src/main/java/com/sesac/msa/orderservice/client/ProductServiceClient.java
+++ backend/order-service/src/main/java/com/sesac/msa/orderservice/client/ProductServiceClient.java
@@ -3,13 +3,12 @@ package com.sesac.msa.orderservice.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sesac.msa.orderservice.client.dto.ProductResponse;
 
 @FeignClient(name = "product-service")
-@RequestMapping("/api/v1/products")
 public interface  ProductServiceClient {
-	@GetMapping("/{id}")
+	String BASE_URL = "/api/v1/products";
+	@GetMapping(BASE_URL+"/{id}")
 	ProductResponse getProductById(@PathVariable Long id);
 }
--- backend/order-service/src/main/java/com/sesac/msa/orderservice/client/UserServiceClient.java
+++ backend/order-service/src/main/java/com/sesac/msa/orderservice/client/UserServiceClient.java
@@ -3,13 +3,12 @@ package com.sesac.msa.orderservice.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sesac.msa.orderservice.client.dto.UserResponse;
 
 @FeignClient(name = "user-service")
-@RequestMapping("/api/v1/users")
 public interface UserServiceClient {
-	@GetMapping("/{id}")
+	String BASE_URL = "/api/v1/users";
+	@GetMapping(BASE_URL+"/{id}")
 	UserResponse getUserById(@PathVariable Long id);
 }
