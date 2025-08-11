bdbd65094e07e6b3c02812286a4ad44298fa93ab
namung08 <namung08@gmail.com>
2025-08-11 15:05:51 +0900

    :sparkles: feat: 로그인 서비스 선언 및 구현
    Signed-off-by: namung08 <namung08@gmail.com>

--- backend/user-service/src/main/java/com/sesac/msa/userservice/service/UserService.java
+++ backend/user-service/src/main/java/com/sesac/msa/userservice/service/UserService.java
@@ -1,7 +1,11 @@
 package com.sesac.msa.userservice.service;
 
+import com.sesac.msa.userservice.dto.LoginRequest;
+import com.sesac.msa.userservice.dto.LoginResponse;
 import com.sesac.msa.userservice.entity.User;
 
 public interface UserService {
 	User findById(Long id);
+
+	LoginResponse login(LoginRequest loginRequest);
 }
